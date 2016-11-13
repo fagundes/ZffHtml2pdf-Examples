baa2f5acbe67f92c51b7d54376f76e197c33a04b
Vinícius Fagundes <mvlacerda@gmail.com>
2016-11-13 18:19:34 -0200

    create example 01

--- module/Application/src/Controller/ExampleController.php
+++ module/Application/src/Controller/ExampleController.php
@@ -9,15 +9,7 @@ class ExampleController extends AbstractActionController
 {
     public function example00Action()
     {
-
-        /**
-         * EXAMPLE 00
-         */
-
         $html2pdfModel = new Html2PdfModel();
-
-//        $html2pdf->setDefaultFont('Arial');
-
         $html2pdfModel->setHtml2PdfOptions([
             'orientation' => 'P',
             'format'      => 'A4',
@@ -25,6 +17,21 @@ class ExampleController extends AbstractActionController
             'margins'     => [5, 5, 5, 5]
         ]);
         $html2pdfModel->setFilename('exemple00.pdf');
+//        $html2pdf->setDefaultFont('Arial');
+
+        return $html2pdfModel;
+    }
+
+    public function example01Action()
+    {
+        $html2pdfModel = new Html2PdfModel();
+        $html2pdfModel->setHtml2PdfOptions([
+            'orientation' => 'P',
+            'format'      => 'A4',
+            'lang'        => 'fr',
+            'margins'     => [5, 5, 5, 5]
+        ]);
+        $html2pdfModel->setFilename('exemple01.pdf');
 
         return $html2pdfModel;
     }
