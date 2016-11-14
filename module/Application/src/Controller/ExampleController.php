66e48017452585f0eb9048fa45ce53e72e7c027b
Vinícius Fagundes <mvlacerda@gmail.com>
2016-11-14 00:30:51 -0200

    create examples 07, 08, 09 and 10

--- module/Application/src/Controller/ExampleController.php
+++ module/Application/src/Controller/ExampleController.php
@@ -16,7 +16,7 @@ class ExampleController extends AbstractActionController
             'lang'        => 'fr',
             'margins'     => [5, 5, 5, 5]
         ]);
-        $html2pdfModel->setFilename('exemple00.pdf');
+        $html2pdfModel->setFilename('example00.pdf');
 //        $html2pdf->setDefaultFont('Arial');
 
         return $html2pdfModel;
@@ -31,7 +31,7 @@ class ExampleController extends AbstractActionController
             'lang'        => 'fr',
             'margins'     => [5, 5, 5, 5]
         ]);
-        $html2pdfModel->setFilename('exemple01.pdf');
+        $html2pdfModel->setFilename('example01.pdf');
 
         return $html2pdfModel;
     }
@@ -47,7 +47,7 @@ class ExampleController extends AbstractActionController
             'encoding'    => 'UTF-8',
             'margins'     => [15, 5, 15, 5]
         ]);
-        $html2pdfModel->setFilename('exemple02.pdf');
+        $html2pdfModel->setFilename('example02.pdf');
 //        $html2pdf->pdf->SetDisplayMode('fullpage');
 
         return $html2pdfModel;
@@ -64,7 +64,7 @@ class ExampleController extends AbstractActionController
             'encoding'    => 'UTF-8',
             'margins'     => 3
         ]);
-        $html2pdfModel->setFilename('exemple03.pdf');
+        $html2pdfModel->setFilename('example03.pdf');
 //        $html2pdf->pdf->SetDisplayMode('fullpage');
 
         return $html2pdfModel;
@@ -79,7 +79,7 @@ class ExampleController extends AbstractActionController
             'lang'        => 'fr',
             'margins'     => [5, 5, 5, 5]
         ]);
-        $html2pdfModel->setFilename('exemple04.pdf');
+        $html2pdfModel->setFilename('example04.pdf');
 //        $html2pdf->pdf->SetDisplayMode('fullpage');
 
         return $html2pdfModel;
@@ -89,7 +89,7 @@ class ExampleController extends AbstractActionController
     {
         $html2pdfModel = $this->example04Action();
 
-        $html2pdfModel->setFilename('exemple05.pdf');
+        $html2pdfModel->setFilename('example05.pdf');
 
         return $html2pdfModel;
     }
@@ -98,7 +98,85 @@ class ExampleController extends AbstractActionController
     {
         $html2pdfModel = $this->example04Action();
 
-        $html2pdfModel->setFilename('exemple06.pdf');
+        $html2pdfModel->setFilename('example06.pdf');
+
+        return $html2pdfModel;
+    }
+
+    public function example07Action()
+    {
+        $html2pdfModel = $this->example04Action();
+
+        $html2pdfModel->setFilename('example07.pdf');
+
+        return $html2pdfModel;
+    }
+
+    public function example08Action()
+    {
+        $html2pdfModel = new Html2PdfModel();
+        $html2pdfModel->setHtml2PdfOptions([
+            'orientation' => 'P',
+            'format'      => 'A4',
+            'lang'        => 'fr',
+        ]);
+
+        $html2pdfModel->setFilename('example08.pdf');
+
+        return $html2pdfModel;
+    }
+
+    public function example09Action()
+    {
+        $request = $this->getRequest();
+
+        $httpHost = $request->getServer('HTTP_HOST');
+        $url      = 'http://' . $httpHost . $this->url()->fromRoute('example', ['action' => 'example09-png'], ['query' => ['px' => 5, 'py' => 20]]);
+
+        if ($request->getQuery('make_pdf', false) !== false) {
+            $make_pdf = $request->getQuery('make_pdf');
+            $nom      = $request->getQuery('nom');
+
+            $nom = substr(preg_replace('/[^a-zA-Z0-9]/isU', '', $nom), 0, 26);
+
+            $html2pdfModel = new Html2PdfModel([
+                'generate' => true,
+                'make_pdf' => $make_pdf,
+                'nom'      => $nom,
+                'url'      => $url,
+            ]);
+            $html2pdfModel->setHtml2PdfOptions([
+                'orientation' => 'P',
+                'format'      => 'A4',
+                'lang'        => 'fr',
+                'margins'     => [5, 5, 5, 5],
+            ]);
+
+            $html2pdfModel->setFilename('example09.pdf');
+
+            return $html2pdfModel;
+        }
+
+        return [
+            'url'      => $url,
+            'generate' => false,
+        ];
+    }
+
+    public function example09PngAction()
+    {
+        $this->layout('layout/png');
+        return [
+            'px' => preg_replace('/[^0-9]/isU', '', $this->getRequest()->getQuery('px', 0)),
+            'py' => preg_replace('/[^0-9]/isU', '', $this->getRequest()->getQuery('py', 0)),
+        ];
+    }
+
+    public function example10Action()
+    {
+        $html2pdfModel = $this->example04Action();
+
+        $html2pdfModel->setFilename('example10.pdf');
 
         return $html2pdfModel;
     }
